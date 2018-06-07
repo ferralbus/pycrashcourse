978fcec3dc78fc86e70b5f2cb82a35e1f182e804
ferralbus <fernando@torrico.eu>
2018-06-07 19:27:41 +0200

    Update bullet.py

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -1,2 +1,11 @@
-class Bullet():
+import pygame
+from pygame import Sprite
+
+class Bullet(Sprite):
+	def __init__(self,screen,ai_settings):
+		super().__init__()
+		self.screen = screen
+		#Crear un Rect en (0,0) para la bala y luego colocarlo en
+		#la parte de arriba de la nave
+		self.rect = pygame.Rect(0,0,ai_settings.
 	
